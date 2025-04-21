103a20b11f2e1945ab6cc3d1672a0d58c92a5494
Yashveer <yashveersingh83@gmail.com>
2025-04-21 20:30:01 +0200

    updated  keyclock url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://host.docker.internal:8010/api', // Example API URL for Docker
+    apiUrl: 'http://localhost:8010/api', // Example API URL for Docker
     enableDebug: false,
     realm:'Datawrangler',
     keyclockClientId:'datawranglerclient',
-    keyclockServerUrl:'http://host.docker.internal:7080/',
+    keyclockServerUrl:'http://localhost:7080/',
     localHostUrlPattern:/^(http:\/\/localhost:8080)(\/.*)?$/i
   };
